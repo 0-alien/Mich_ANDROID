79a7c294106ed0d8e122c5af2397fefb8429e877
sabagogolidze <sggol11@freeuni.edu.ge>
2016-12-14 16:08:44 +0400

    chat

--- app/src/main/java/com/mich/android/mich/activities/VsDetailsActivity.java
+++ app/src/main/java/com/mich/android/mich/activities/VsDetailsActivity.java
@@ -3,6 +3,7 @@ package com.mich.android.mich.activities;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.mich.android.mich.BaseActivity;
 import com.mich.android.mich.R;
@@ -15,6 +16,12 @@ public class VsDetailsActivity extends BaseActivity {
         RecyclerView recyclerView = (RecyclerView)findViewById(R.id.list);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new VsDetailsAdapter());
+        findViewById(R.id.back_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 
     @Override
